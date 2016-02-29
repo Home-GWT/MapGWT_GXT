c69d7bfadf11dd940e6f83e43b016bad78b00548
sashakmets <sashakmets@yandex.ru>
2016-02-29 15:35:03 +0300

    develop-2.0

--- src/main/java/com/demo/gxt_google_maps/client/view2/model/Contact.java
+++ src/main/java/com/demo/gxt_google_maps/client/view2/model/Contact.java
@@ -1,210 +1,210 @@
-package com.demo.gxt_google_maps.client.view2.model;
-
-import java.io.Serializable;
-
-public class Contact implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-	private Integer id;
-	private String name;
-	private String phone;
-	private String email;
-
-    private String firstName;
-    private String lastName;
-    private String time;
-    private String timePosition;
-    private String timeServer;
-    private Integer speed;
-    private Integer height;
-    private String model;
-    private String number;
-    private Integer distance;
-    private String position;
-    private Integer degree;
-    private String vehicleType;
-    private Boolean wifi;
-    private MyObject object;
-
-	public Contact(){
-		super();
-	}
-	
-	public Contact(Integer id, String name, String phone, String email) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.phone = phone;
-		this.email = email;
-	}
-
-    public Contact(Integer id, String name, String phone, String email, String firstName, String lastName, String time, String timePosition, String timeServer, Integer speed, Integer height, String model, String number, Integer distance, String position, Integer degree, String vehicleType, Boolean wifi) {
-        super();
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.time = time;
-        this.timePosition = timePosition;
-        this.timeServer = timeServer;
-        this.speed = speed;
-        this.height = height;
-        this.model = model;
-        this.number = number;
-        this.distance = distance;
-        this.position = position;
-        this.degree = degree;
-        this.vehicleType = vehicleType;
-        this.wifi = wifi;
-        object = new MyObject(time, vehicleType);
-    }
-	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getTimePosition() {
-        return timePosition;
-    }
-
-    public void setTimePosition(String timePosition) {
-        this.timePosition = timePosition;
-    }
-
-    public String getTimeServer() {
-        return timeServer;
-    }
-
-    public void setTimeServer(String timeServer) {
-        this.timeServer = timeServer;
-    }
-
-    public Integer getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(Integer speed) {
-        this.speed = speed;
-    }
-
-    public Integer getHeight() {
-        return height;
-    }
-
-    public void setHeight(Integer height) {
-        this.height = height;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public Integer getDistance() {
-        return distance;
-    }
-
-    public void setDistance(Integer distance) {
-        this.distance = distance;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public Integer getDegree() {
-        return degree;
-    }
-
-    public void setDegree(Integer degree) {
-        this.degree = degree;
-    }
-
-    public String getVehicleType() {
-        return vehicleType;
-    }
-
-    public void setVehicleType(String vehicleType) {
-        this.vehicleType = vehicleType;
-    }
-
-    public Boolean isWifi() {
-        return wifi;
-    }
-
-    public void setWifi(Boolean wifi) {
-        this.wifi = wifi;
-    }
-
-    public MyObject getObject() {
-        return object;
-    }
-
-    public void setObject(MyObject object) {
-        this.object = object;
-    }
-}
+//package com.demo.gxt_google_maps.client.view2.model;
+//
+//import java.io.Serializable;
+//
+//public class Contact implements Serializable {
+//
+//	private static final long serialVersionUID = 1L;
+//
+//	private Integer id;
+//	private String name;
+//	private String phone;
+//	private String email;
+//
+//    private String firstName;
+//    private String lastName;
+//    private String time;
+//    private String timePosition;
+//    private String timeServer;
+//    private Integer speed;
+//    private Integer height;
+//    private String model;
+//    private String number;
+//    private Integer distance;
+//    private String position;
+//    private Integer degree;
+//    private String vehicleType;
+//    private Boolean wifi;
+//    private MyObject object;
+//
+//	public Contact(){
+//		super();
+//	}
+//
+//	public Contact(Integer id, String name, String phone, String email) {
+//		super();
+//		this.id = id;
+//		this.name = name;
+//		this.phone = phone;
+//		this.email = email;
+//	}
+//
+//    public Contact(Integer id, String name, String phone, String email, String firstName, String lastName, String time, String timePosition, String timeServer, Integer speed, Integer height, String model, String number, Integer distance, String position, Integer degree, String vehicleType, Boolean wifi) {
+//        super();
+//        this.id = id;
+//        this.name = name;
+//        this.phone = phone;
+//        this.email = email;
+//
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.time = time;
+//        this.timePosition = timePosition;
+//        this.timeServer = timeServer;
+//        this.speed = speed;
+//        this.height = height;
+//        this.model = model;
+//        this.number = number;
+//        this.distance = distance;
+//        this.position = position;
+//        this.degree = degree;
+//        this.vehicleType = vehicleType;
+//        this.wifi = wifi;
+//        object = new MyObject(time, vehicleType);
+//    }
+//
+//	public Integer getId() {
+//		return id;
+//	}
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public String getPhone() {
+//		return phone;
+//	}
+//	public void setPhone(String phone) {
+//		this.phone = phone;
+//	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getTime() {
+//        return time;
+//    }
+//
+//    public void setTime(String time) {
+//        this.time = time;
+//    }
+//
+//    public String getTimePosition() {
+//        return timePosition;
+//    }
+//
+//    public void setTimePosition(String timePosition) {
+//        this.timePosition = timePosition;
+//    }
+//
+//    public String getTimeServer() {
+//        return timeServer;
+//    }
+//
+//    public void setTimeServer(String timeServer) {
+//        this.timeServer = timeServer;
+//    }
+//
+//    public Integer getSpeed() {
+//        return speed;
+//    }
+//
+//    public void setSpeed(Integer speed) {
+//        this.speed = speed;
+//    }
+//
+//    public Integer getHeight() {
+//        return height;
+//    }
+//
+//    public void setHeight(Integer height) {
+//        this.height = height;
+//    }
+//
+//    public String getModel() {
+//        return model;
+//    }
+//
+//    public void setModel(String model) {
+//        this.model = model;
+//    }
+//
+//    public String getNumber() {
+//        return number;
+//    }
+//
+//    public void setNumber(String number) {
+//        this.number = number;
+//    }
+//
+//    public Integer getDistance() {
+//        return distance;
+//    }
+//
+//    public void setDistance(Integer distance) {
+//        this.distance = distance;
+//    }
+//
+//    public String getPosition() {
+//        return position;
+//    }
+//
+//    public void setPosition(String position) {
+//        this.position = position;
+//    }
+//
+//    public Integer getDegree() {
+//        return degree;
+//    }
+//
+//    public void setDegree(Integer degree) {
+//        this.degree = degree;
+//    }
+//
+//    public String getVehicleType() {
+//        return vehicleType;
+//    }
+//
+//    public void setVehicleType(String vehicleType) {
+//        this.vehicleType = vehicleType;
+//    }
+//
+//    public Boolean isWifi() {
+//        return wifi;
+//    }
+//
+//    public void setWifi(Boolean wifi) {
+//        this.wifi = wifi;
+//    }
+//
+//    public MyObject getObject() {
+//        return object;
+//    }
+//
+//    public void setObject(MyObject object) {
+//        this.object = object;
+//    }
+//}
--- src/main/java/com/demo/gxt_google_maps/client/view2/model/ContactProperties.java
+++ src/main/java/com/demo/gxt_google_maps/client/view2/model/ContactProperties.java
@@ -1,35 +1,35 @@
-package com.demo.gxt_google_maps.client.view2.model;
-
-import com.google.gwt.editor.client.Editor.Path;
-import com.sencha.gxt.core.client.ValueProvider;
-import com.sencha.gxt.data.shared.ModelKeyProvider;
-import com.sencha.gxt.data.shared.PropertyAccess;
-
-public interface ContactProperties extends PropertyAccess<Contact> {
-
-	@Path("id")
-	ModelKeyProvider<Contact> key();
-	
-	ValueProvider<Contact, String> name();
-	
-	ValueProvider<Contact, String> phone();
-	
-	ValueProvider<Contact, String> email();
-
-
-    ValueProvider<Contact, String> firstName();
-    ValueProvider<Contact, String> lastName();
-    ValueProvider<Contact, String> time();
-    ValueProvider<Contact, String> timePosition();
-    ValueProvider<Contact, String> timeServer();
-    ValueProvider<Contact, Integer> speed();
-    ValueProvider<Contact, Integer> height();
-    ValueProvider<Contact, String> model();
-    ValueProvider<Contact, String> number();
-    ValueProvider<Contact, Integer> distance();
-    ValueProvider<Contact, String> position();
-    ValueProvider<Contact, Integer> degree();
-    ValueProvider<Contact, Integer> vehicleType();
-    ValueProvider<Contact, Boolean> wifi();
-    ValueProvider<Contact, MyObject> object();
-}
+//package com.demo.gxt_google_maps.client.view2.model;
+//
+//import com.google.gwt.editor.client.Editor.Path;
+//import com.sencha.gxt.core.client.ValueProvider;
+//import com.sencha.gxt.data.shared.ModelKeyProvider;
+//import com.sencha.gxt.data.shared.PropertyAccess;
+//
+//public interface ContactProperties extends PropertyAccess<Contact> {
+//
+//	@Path("id")
+//	ModelKeyProvider<Contact> key();
+//
+//	ValueProvider<Contact, String> name();
+//
+//	ValueProvider<Contact, String> phone();
+//
+//	ValueProvider<Contact, String> email();
+//
+//
+//    ValueProvider<Contact, String> firstName();
+//    ValueProvider<Contact, String> lastName();
+//    ValueProvider<Contact, String> time();
+//    ValueProvider<Contact, String> timePosition();
+//    ValueProvider<Contact, String> timeServer();
+//    ValueProvider<Contact, Integer> speed();
+//    ValueProvider<Contact, Integer> height();
+//    ValueProvider<Contact, String> model();
+//    ValueProvider<Contact, String> number();
+//    ValueProvider<Contact, Integer> distance();
+//    ValueProvider<Contact, String> position();
+//    ValueProvider<Contact, Integer> degree();
+//    ValueProvider<Contact, Integer> vehicleType();
+//    ValueProvider<Contact, Boolean> wifi();
+//    ValueProvider<Contact, MyObject> object();
+//}
