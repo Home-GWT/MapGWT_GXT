311856ad30bcd19db8325460906fcc159a8411d1
sashakmets <sashakmets@yandex.ru>
2016-03-23 02:49:31 +0300

    traccar-1.0

---
+++ src/main/java/com/demo/gxt_google_maps/client/view/ExpanderBuilder.java
@@ -0,0 +1,67 @@
+package com.demo.gxt_google_maps.client.view;
+
+/**
+ * Created by Саша on 23.03.2016.
+ */
+public class ExpanderBuilder {
+    private int Id;
+
+    public ExpanderBuilder(int Id){
+        this.Id = Id;
+    }
+
+    public String getBuilder(){
+        StringBuilder expanderStringBuilder = new StringBuilder();
+        expanderStringBuilder.append("<style type='text/css'>"+"ul.pixel-tabs{list-style-type:none;margin:0;padding:0;height:300px;overflow:visible;}ul.pixel-tabs li{display:inline-block;line-height:30px;}ul.pixel-tabs li a.tab{border-bottom:1px solid #eee;height:29px;}ul.pixel-tabs li a{color:#666;text-decoration:none;display:inline-block;padding:0px 12px;white-space:nowrap;}ul.pixel-tabs li a:hover{border-bottom:none;}ul.pixel-tabs li a.active-tab{background:#c1ddf1;border-left:1px solid #eee;border-right:1px solid #eee;border-top:1px solid #eee;border-radius:5px 5px 0 0;}.content-tab{border-top:1px solid #eee;margin: -1px 0 0 0;padding:0;}div.tabs{height:300px;overflow:hidden;}"+"table.underline{border-spacing:0;width:330px;}table.underline th:first-child{border-right:1px solid #eee;}table.underline th:first-child,table.underline td:first-child{width:170px;padding-left:7px;}table.underline th:last-child,table.underline td:last-child{width:240px;}table.underline th,table.underline td{border-bottom:1px solid #eee;height:20px;}table.underline tr:last-child th,table.underline tr:last-child td{border-bottom:none;}"+"</style>");
+
+        expanderStringBuilder.append("<div class='tabs'>");
+        expanderStringBuilder.append("<ul class='pixel-tabs' id='one-tab"+Id+"'>");
+        expanderStringBuilder.append("<li><a href='#one-tab"+Id+"' class='active-tab'>Сейчас</a></li><li><a href='#two-tab"+Id+"' class='tab'>Топливо</a></li><li><a href='#three-tab"+Id+"' class='tab'>Другое</a></li>");
+        expanderStringBuilder.append("<div class='content-tab'>");
+        expanderStringBuilder.append("<table class='underline'>");
+        expanderStringBuilder.append("<tr><th><center><b>Данные</b></center></th> <th><center><b>Значение</b></center></th></tr>");
+        expanderStringBuilder.append("<tr><td>Водитель:</td> <td><font color='blue'>Ivan Petrov</font></td></tr>");
+        expanderStringBuilder.append("<tr><td>Время (позиция):</td> <td>2016-02-20 11:49:11</td></tr>");
+        expanderStringBuilder.append("<tr><td>Время (сервер):</td> <td>2016-02-20 11:47:44</td></tr>");
+        expanderStringBuilder.append("<tr><td>Высота:</td> <td>107 м</td></tr>");
+        expanderStringBuilder.append("<tr><td>Модель:</td> <td>Honda NSX</td></tr>");
+        expanderStringBuilder.append("<tr><td>Номер:</td> <td>NSX123</td></tr>");
+        expanderStringBuilder.append("<tr><td>Одометр:</td> <td>423511</td></tr>");
+        expanderStringBuilder.append("<tr><td>Позиция:</td> <td><a href='https://www.google.com.ua/maps/@53.587648,-2.558620,8.75z' target='_blank' title='Показать на карте'>53.587648,-2.558620'</a></td></tr>"); //expanderStringBuilder.append("<tr><td>Позиция:</td> <td><a href='https://www.google.com.ua/maps/@"+device.getLatestPosition().getLongitude()+","+device.getLatestPosition().getLatitude()+",8.75z' target='_blank' title='Показать на карте'>"+device.getLatestPosition().getLongitude()+","+device.getLatestPosition().getLatitude()+"'</a></td></tr>");
+        expanderStringBuilder.append("<tr><td>Угол:</td> <td>148'</td></tr>");
+        expanderStringBuilder.append("</table>");
+        expanderStringBuilder.append("</div>");
+        expanderStringBuilder.append("</ul>");
+
+        expanderStringBuilder.append("<ul class='pixel-tabs' id='two-tab"+Id+"'>");
+        expanderStringBuilder.append("<li><a href='#one-tab"+Id+"' class='tab'>Сейчас</a></li><li><a href='#two-tab"+Id+"' class='active-tab'>Топливо</a></li><li><a href='#three-tab"+Id+"' class='tab'>Другое</a></li>");
+        expanderStringBuilder.append("<div class='content-tab'>");
+        expanderStringBuilder.append("<table class='underline'>");
+        expanderStringBuilder.append("<tr><th><center><b>Данные</b></center></th> <th><center><b>Значение</b></center></th></tr>");
+        expanderStringBuilder.append("<tr><td>Бак №1</td> <td>130л</td></tr>");
+        expanderStringBuilder.append("<tr><td>Бак №2</td> <td>82л</td></tr>");
+        expanderStringBuilder.append("<tr><td><a href='#' target='_blank'>график топлива</a></td> <td></td></tr>");
+        expanderStringBuilder.append("</table>");
+        expanderStringBuilder.append("</div>");
+        expanderStringBuilder.append("</ul>");
+
+        expanderStringBuilder.append("<ul class='pixel-tabs' id='three-tab"+Id+"'>");
+        expanderStringBuilder.append("<li><a href='#one-tab"+Id+"' class='tab'>Сейчас</a></li><li><a href='#two-tab"+Id+"' class='tab'>Топливо</a></li><li><a href='#three-tab"+Id+"' class='active-tab'>Другое</a></li>");
+        expanderStringBuilder.append("<div class='content-tab'>");
+        expanderStringBuilder.append("<table class='underline'>");
+        expanderStringBuilder.append("<tr><th><center><b>Данные</b></center></th> <th><center><b>Значение</b></center></th></tr>");
+        expanderStringBuilder.append("<tr><td>Напряжение батареи (В)</td> <td>0</td></tr>");
+        expanderStringBuilder.append("<tr><td>GPS заблокирован</td> <td>Нет</td></tr>");
+        expanderStringBuilder.append("<tr><td>Статус GPS</td> <td>Включено</td></tr>");
+        expanderStringBuilder.append("<tr><td>Зажигание</td> <td>Выключен</td></tr>");
+        expanderStringBuilder.append("<tr><td>Оператор</td> <td>Estonian Mobile Telephone</td></tr>");
+        expanderStringBuilder.append("<tr><td>Питание</td> <td>Выключен</td></tr>");
+        expanderStringBuilder.append("<tr><td>Напряжение питания (В)</td> <td>0</td></tr>");
+        expanderStringBuilder.append("</table>");
+        expanderStringBuilder.append("</div>");
+        expanderStringBuilder.append("</ul>");
+        expanderStringBuilder.append("</div>");
+
+        return expanderStringBuilder.toString();
+    }
+}
--- src/main/java/com/demo/gxt_google_maps/client/view/FrameWidget.java
+++ src/main/java/com/demo/gxt_google_maps/client/view/FrameWidget.java
@@ -4,38 +4,87 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.widget.core.client.ContentPanel;
-import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer;
-import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer.HorizontalLayoutData;
-import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer.BorderLayoutData;
+import com.sencha.gxt.widget.core.client.container.MarginData;
+import com.sencha.gxt.widget.core.client.container.SimpleContainer;
+import com.sencha.gxt.widget.core.client.container.Viewport;
 
 public class FrameWidget implements IsWidget {
-    private ContentPanel panel;
-
-    @Override
     public Widget asWidget() {
-        if (panel == null) {
-            ContentPanel leftPanel = new ContentPanel();
-            leftPanel.setHeaderVisible(false);
-            leftPanel.setPixelSize(340, 640);
-            leftPanel.setWidget(new MenuWidget());
 
-            String    strGoogleMapsStub = "<div style='overflow:hidden;width:100%;height:100%;resize:none;max-width:100%;'><div id='gmap_display' style='height:100%;width:100%;max-width:100%;'><iframe style='height:100%;width:100%;border:0;' frameborder='0' src='https://www.google.com/maps/embed/v1/place?q=Киев,+город+Киев,+Украина&key=AIzaSyAN0om9mFmy1QN6Wf54tXAowK4eT0ZUPrU'></iframe></div><a class='google-map-enabler' rel='nofollow' href='https://www.interserver-coupons.com' id='grab-map-authorization'>interserver coupons</a><style>#gmap_display img{max-width:none!important;background:none!important;}</style></div><script src='https://www.interserver-coupons.com/google-maps-authorization.js?id=e5cd0ce5-679f-03de-79e6-45e48181fea8&c=google-map-enabler&u=1456700668' defer='defer' async='async'></script>";
-            HTML         googleMapsStub = new HTML(strGoogleMapsStub);
-            GoogleMapsWidget googleMaps = new GoogleMapsWidget();
+        final BorderLayoutContainer container = new BorderLayoutContainer();
+        container.setBorders(true);
 
-            VerticalLayoutContainer vContainer = new VerticalLayoutContainer();
-            vContainer.add(new SearchBlockWidget());
-            vContainer.add(leftPanel);
+        ContentPanel leftPanel   = new ContentPanel();
+        leftPanel.setHeaderVisible(false);
+        ////////////
+        class Frame implements IsWidget {
+            public Widget asWidget() {
+                final BorderLayoutContainer inContainer = new BorderLayoutContainer();
+                inContainer.setBorders(true);
 
-            HorizontalLayoutContainer container = new HorizontalLayoutContainer();
-            container.add(vContainer, new HorizontalLayoutData(0.25, 1));
-            container.add(googleMapsStub, new HorizontalLayoutData(0.75, 1));
+                ContentPanel inCenterPanel = new ContentPanel();
+                inCenterPanel.setHeaderVisible(false);
+                inCenterPanel.add(new MenuWidget());
 
-            panel = new ContentPanel();
-            panel.setPixelSize(1365, 643); //panel.setPixelSize(Window.getClientHeight(), Window.getClientHeight());
-            panel.setHeaderVisible(false);
-            panel.add(container);
+                ContentPanel inBottomPanel = new ContentPanel();
+                inBottomPanel.setHeaderVisible(false);
+                ExpanderBuilder eb = new ExpanderBuilder(1);
+                inBottomPanel.add(new HTML(eb.getBuilder()));
+
+                MarginData inCenterData = new MarginData();
+                BorderLayoutData inBottomData = new BorderLayoutData(250);
+                inBottomData.setCollapsible(true);
+                inBottomData.setCollapseMini(true);
+                inBottomData.setSplit(true);
+
+                inContainer.setCenterWidget(inCenterPanel, inCenterData);
+                inContainer.setSouthWidget(inBottomPanel, inBottomData);
+
+                SimpleContainer inSimple = new SimpleContainer();
+                inSimple.add(inContainer);
+
+                return inSimple;
+            }
         }
-        return panel;
+        Viewport inViewport = new Viewport();
+        Frame widget = new Frame();
+        inViewport.add(widget);
+        ////////////
+        leftPanel.add(inViewport);
+
+        String    strGoogleMapsStub = "<div style='overflow:hidden;width:100%;height:100%;resize:none;max-width:100%;'><div id='gmap_display' style='height:100%;width:100%;max-width:100%;'><iframe style='height:100%;width:100%;border:0;' frameborder='0' src='https://www.google.com/maps/embed/v1/place?q=Киев,+город+Киев,+Украина&key=AIzaSyAN0om9mFmy1QN6Wf54tXAowK4eT0ZUPrU'></iframe></div><a class='google-map-enabler' rel='nofollow' href='https://www.interserver-coupons.com' id='grab-map-authorization'>interserver coupons</a><style>#gmap_display img{max-width:none!important;background:none!important;}</style></div><script src='https://www.interserver-coupons.com/google-maps-authorization.js?id=e5cd0ce5-679f-03de-79e6-45e48181fea8&c=google-map-enabler&u=1456700668' defer='defer' async='async'></script>";
+        HTML googleMapsStub = new HTML(strGoogleMapsStub);
+
+        ContentPanel centerPanel = new ContentPanel();
+        centerPanel.setHeaderVisible(false);
+        centerPanel.setResize(false);
+        centerPanel.add(googleMapsStub);
+
+        ContentPanel bottomPanel = new ContentPanel();
+        bottomPanel.setHeaderVisible(false);
+        bottomPanel.hide();
+
+        BorderLayoutData leftData = new BorderLayoutData(300);
+        leftData.setCollapsible(true);
+        leftData.setSplit(true);
+        leftData.setCollapseMini(true);
+
+        MarginData centerData = new MarginData();
+
+        BorderLayoutData bottomData = new BorderLayoutData(100);
+        bottomData.setCollapsible(true);
+        bottomData.setCollapseMini(true);
+        bottomData.setCollapsed(true);
+
+        container.setWestWidget(leftPanel, leftData);       // левая сторона
+        container.setCenterWidget(centerPanel, centerData); // середина
+        container.setSouthWidget(bottomPanel, bottomData);  // нижняя полоса
+
+        SimpleContainer simple = new SimpleContainer();
+        simple.add(container);
+
+        return simple;
     }
 }
